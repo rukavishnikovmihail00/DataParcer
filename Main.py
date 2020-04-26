756a291e798619f05082f1b0c41132bc5ca2f8b4
Mikhail Rukavishnikov <61332555+rukavishnikovmihail00@users.noreply.github.com>
2020-04-26 17:52:49 +0300

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,8 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as BS
 
-URL = 'https://vk.com/rukavishnikov_mishka'
-
+URL = 'https://ru.wikipedia.org/wiki/Россия'
 def get_url(url):
     r = requests.get(url)
     return r
@@ -11,8 +10,14 @@ def get_url(url):
 def parse():
     html = get_url(URL)
     if html.status_code == 200:
-        pass
+        get_content(html)
     else:
         print("Something went wrong")
 
+
+def get_content(html):
+    soup = BS(html, 'lxml')
+    links = {}
+    link = soup.find('a', links)
+    print(links)
 parse()
